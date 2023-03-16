f65c4c1a4f1b401c7655d88808614e7c2fbb608f
vshchutc <victoria.schutska@gmail.com>
2023-03-16 20:33:23 +0100

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 import ContainerToTrack from './ContainerToTrack';
-import './App.css';
 
 function App() {
   const [history, setHistory] = useState<{ x: number, y: number}[]>([])
   const [isCounterOn, setIsCounterOn] = useState(false);
 
   return (
-    <div className="App">
-      <ContainerToTrack history={history} setHistory={setHistory} isCounterOn={isCounterOn} setIsCounterOn={setIsCounterOn} />
-      isOn {isCounterOn + ''} {history.length}
-    <br/>
-      {!isCounterOn && history.map((pos) => <div><span>{pos.x}</span> <span>{pos.y}</span></div>)}
+    <div>
+      <ContainerToTrack
+        history={history}
+        setHistory={setHistory}
+        isCounterOn={isCounterOn}
+        setIsCounterOn={setIsCounterOn}
+      />
+      {isCounterOn && "Calculating..."}
+
+      {!isCounterOn && history.map((pos) => <span>[ X:{pos.x} Y:{pos.y}],</span>)}
+      
     </div>
   );
 }
